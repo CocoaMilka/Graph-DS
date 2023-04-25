328ad26a9ab977f74fe5da094dc7ff2ff6ce65ff
jrgbk4 <jrgbk4@umsystem.edu>
2023-04-24 23:08:43 -0500

    Finished :3

--- graph.py
+++ graph.py
@@ -4,26 +4,25 @@ class Graph:
     # Member Variables
     vertices = []
     adjacentList = {}
+    adjacentCount = {}
+    parent = {}
     visited = {}
-    level = 0
+    level = {}
 
     # Takes an array of vertices as input
     def __init__(self, vertexList):
         for i in vertexList:
             self.vertices.append(i)
             self.adjacentList[i] = []
+            self.adjacentCount[i] = 0
             self.visited[i] = -1
+            self.level[i] = 0
     
     # x is the starting vertex and y is the ending vertex
     def addEdge(self, x, y):
         self.adjacentList[x].append(y)
         self.adjacentList[y].append(x)
 
-    def breadthFirstSearch(self):
-        for i in 
-
-    #def depthFirstSearch(self):
-        # stuff
 
     # Debug functions
     def printVertices(self):
@@ -37,3 +36,46 @@ class Graph:
     def printVisited(self):
         print("Visited List: ")
         print(self.visited)
+
+    def printLevel(self):
+        print("Level List: ")
+        print(self.level)
+
+
+# External Graph Operations
+
+def breadthFirstSearch(s, G):
+    frontier = []
+    G.visited[s] = 0
+    frontier.append(s)
+
+    while frontier != []:
+        u = frontier.pop(0)
+        for v in G.adjacentList[u]:
+            if G.visited[v] == -1:
+                G.visited[v] = 0
+                G.level[v] = G.level[u] + 1
+                G.parent[v] = u
+                frontier.append(v)
+        G.visited[u] = 1
+    
+    print(G.level)
+
+def depthFirstSearch(s, G):
+    frontier = []
+    G.visited[s] = 0
+    frontier.append(s)
+
+    while frontier != []:
+        u = frontier[len(frontier) - 1]
+        for v in G.adjacentList[u]:
+            if G.visited[v] == -1:
+                G.visited[v] = 0
+                G.parent[v] = u
+                frontier.append(v)
+                break
+            else:
+                G.adjacentCount[u] += 1
+        if G.adjacentCount[u] == len(G.adjacentList[u]):
+            G.visited[u] = 1
+            frontier.pop()
--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 from graph import *
 
-vertices = {1, 2, 3, 4, 5}
+vertices = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 test = Graph(vertices)
 
@@ -8,9 +8,35 @@ test.printVertices()
 
 print("Adding vertices...")
 
+# Graph from P1 H4
+
 test.addEdge(1, 2)
-test.addEdge(1, 4)
-test.addEdge(2, 3)
+test.addEdge(1, 3)
+
+test.addEdge(2, 4)
+test.addEdge(2, 5)
+test.addEdge(2, 6)
+
+test.addEdge(3, 4)
+test.addEdge(3, 5)
+test.addEdge(3, 6)
+
+test.addEdge(4, 7)
+test.addEdge(4, 8)
+
+test.addEdge(5, 7)
+test.addEdge(5, 8)
+
+test.addEdge(6, 7)
+test.addEdge(6, 8)
+
+test.addEdge(7, 9)
+
+test.addEdge(8, 9)
+
 
 test.printAdjacentList()
 test.printVisited()
+
+breadthFirstSearch(1, test)
+depthFirstSearch(1, test)
